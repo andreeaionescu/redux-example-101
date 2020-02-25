f4eddeb7d4ce419704b2e7fb07eae855ce3b6377
Andreea Ionescu <amionescu15@gmail.com>
2020-02-25 23:17:50 +0000

    Create rootReducer with books and visibilityFilter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import rootReducer from './reducers/';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
 
