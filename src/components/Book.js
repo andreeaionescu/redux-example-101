c8f2a29acf21e6b41fd9fa32d2041de2c7dfcc71
Andreea Ionescu <amionescu15@gmail.com>
2020-02-29 20:04:04 +0000

    Added Material UI libaries and listitems

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 
 class Book extends React.Component{
 
     render(){
-        return <li  onClick={this.props.onClick} 
-                    style={{ textDecoration: this.props.completed ? 'line-through' : 'none'}}>
-                    {this.props.title}
-                </li>
+        return <ListItem onClick={this.props.onClick} 
+                         style={{ textDecoration: this.props.completed ? 'line-through' : 'none'}}>
+                    <ListItemText primary={this.props.title} />
+                </ListItem>
           
     }
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import Book from './Book'
+import List from '@material-ui/core/List'
+import { withTheme } from '@material-ui/core/styles';
+
 
 class BookList extends React.Component {
-
     getBookList(){
         if (this.props.books){
-            return <div> { this.props.books.map(book => (
+            return <div> <List component="nav"> { this.props.books.map(book => (
                     <Book key={book.id} {...book} onClick={() => this.props.toggleBook(book.id)} /> )
                     )}
+                   </List> 
                    </div>
         }
     }
 
     render(){
-        return <div>{this.getBookList()}</div>
+        return <div style={{ width: '100%',
+        maxWidth: 360,
+        backgroundColor: this.props.theme.palette.background.paper}}>{this.getBookList()}</div>
     }
 }
 
-export default BookList
+export default withTheme(BookList)
