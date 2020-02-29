55119110645f6bcefc64f247944bc53026adfc84
Andreea Ionescu <amionescu15@gmail.com>
2020-02-29 19:24:52 +0000

    Fixed strike through bug, removed initial state, added redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -3,24 +3,14 @@ import ReactDOM from 'react-dom';
 import rootReducer from './reducers/';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 
-const initialState = {
-    books: [{
-        id: 0,
-        title: 'Design Patterns by GoF',
-        completed: false
-    },
-    {
-        id: 1,
-        title: 'Clean code by Robert Martin',
-        completed: false
-    }],
-    visibilityFilter: 'SHOW_ALL'
-}
-
-const store = createStore(rootReducer, initialState)
+const store = createStore(
+    rootReducer,
+    composeWithDevTools()
+)
 
 ReactDOM.render(
     <Provider store={store}>
